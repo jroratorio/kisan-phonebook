a7e525e1a3ce17119cd3dd0832a6b10a7edb07f5
Sayan Chakraborty <jroratorio@gmail.com>
2018-01-30 11:23:52 +0530

    UI fixes

--- public/js/controllers/ctrlView.js
+++ public/js/controllers/ctrlView.js
@@ -1,5 +1,7 @@
 app.controller('ctrlView', ['$http', '$scope', '$routeParams', function ($http, $scope, $routeParams) {
     $http.get('/phonebook/posts/' + $routeParams.id).success(function (response) {
         $scope.contact = response;
+        
+        $scope.msgUrl = '#/send-message/' + $routeParams.id;
     });
 }]);
